d2ca2a0f99d48f2480ffbe9c247563b41a601178
simnikola@gmail.com <simnikola@gmail.com>
2020-11-23 22:37:55 +0300

    Added method Invoke, which does not close the executing thread

--- HalfAsyncCaller/HalfAsyncCaller/AsyncCaller.cs
+++ HalfAsyncCaller/HalfAsyncCaller/AsyncCaller.cs
@@ -1,26 +1,47 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Reflection;
-using System.Text;
 using System.Threading;
 
 namespace HalfAsyncCaller
 {
-    class AsyncCaller
+    class AsyncCaller:IDisposable
     {
         private EventHandler _h;
         private bool _invokeComplete = false;
-
+        private IAsyncResult _asyncResult;
         public AsyncCaller(EventHandler h)
         {
             _h = h;
+        }
+
+        public void Dispose()
+        {
+            _h.EndInvoke(_asyncResult);
+            _asyncResult.AsyncWaitHandle.Close();
         }
 
         internal bool Invoke(int time, object nullObject, EventArgs emptyEventArgs)
         {
             return Invoke(time);
         }
+
+        /// <summary>
+        /// Этот метод ждет заданный тамаут, затем передает упрвление вызывающему потоку
+        /// </summary>
+        /// <param name="time">Таймаут, мс></param>
+        /// <returns></returns>
         internal bool Invoke(int time)
+        {
+            _asyncResult = _h.BeginInvoke(null, null, null, null);
+            _asyncResult.AsyncWaitHandle.WaitOne(time);
+            return _asyncResult.IsCompleted;
+        }
+
+        /// <summary>
+        /// Этот метод закроет поток с делегатом после тамаута
+        /// </summary>
+        /// <param name="time">Таймаут, мс</param>
+        /// <returns></returns>
+        internal bool InvokeForTime(int time)
         {
             Thread threadInvoke = new Thread(new ThreadStart(EventHandlerThreadInvoke));
             threadInvoke.Start();
@@ -34,7 +55,5 @@ namespace HalfAsyncCaller
             _h.Invoke(null, EventArgs.Empty);
             _invokeComplete = true;
         }
-
-     
     }
 }
